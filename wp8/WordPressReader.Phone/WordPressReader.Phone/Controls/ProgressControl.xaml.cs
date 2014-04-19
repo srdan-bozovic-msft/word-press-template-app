289d785680d2014d5a7b27809dfe72d464c78b16
zmajcek <srdjan.bozovic@live.com>
2014-04-19 13:13:57 +0200

    mod: progress control animation start/stop

--- wp8/WordPressReader.Phone/WordPressReader.Phone/Controls/ProgressControl.xaml.cs
+++ wp8/WordPressReader.Phone/WordPressReader.Phone/Controls/ProgressControl.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.Windows.Data;
 
 namespace WordPressReader.Phone.Controls
 {
@@ -14,10 +15,29 @@ namespace WordPressReader.Phone.Controls
     {
         public ProgressControl()
         {
+            
             InitializeComponent();
 
-            myCortana.Begin();
+            RegisterForNotification("Visibility", this, (d, e) =>
+            {
+                if(Visibility == System.Windows.Visibility.Collapsed)
+                {
+                    myCortana.Stop();
+                }
+                else
+                {
+                    myCortana.Begin();
+                }
+            });
         }
+
+        public void RegisterForNotification(string propertyName, FrameworkElement element, PropertyChangedCallback callback) { 
+            //Bind to a depedency property 
+            Binding b = new Binding(propertyName) { Source = element }; 
+            var prop = System.Windows.DependencyProperty.RegisterAttached( "ListenAttached"+propertyName, typeof(object), typeof(UserControl), new System.Windows.PropertyMetadata(callback)); 
+            element.SetBinding(prop, b); 
+        }
+        //- See more at: http://www.amazedsaint.com/2009/12/silverlight-listening-to-dependency.html#sthash.TjtuKbxp.dpuf
 
 
 
