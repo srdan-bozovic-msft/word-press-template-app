ebb0ee66f59b43c61c99d05353b968824681ce50
zmajcek <srdjan.bozovic@live.com>
2014-04-19 12:49:03 +0200

    add: cortana progress control

---
+++ wp8/WordPressReader.Phone/WordPressReader.Phone/Controls/ProgressControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace WordPressReader.Phone.Controls
+{
+    public partial class ProgressControl : UserControl
+    {
+        public ProgressControl()
+        {
+            InitializeComponent();
+
+            myCortana.Begin();
+        }
+
+
+
+
+    }
+}
