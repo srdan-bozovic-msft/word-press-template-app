38fb0fd038b5f3fd42a1d635b8196313be944469
zmajcek <srdjan.bozovic@live.com>
2014-06-16 00:14:41 +0200

    fix: localization

--- wp8/WordPressReader.Phone/WordPressReader.Phone/Resources/AppResources.Designer.cs
+++ wp8/WordPressReader.Phone/WordPressReader.Phone/Resources/AppResources.Designer.cs
@@ -169,33 +169,6 @@ namespace WordPressReader.Phone.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to add.
-        /// </summary>
-        public static string AppBarButtonText {
-            get {
-                return ResourceManager.GetString("AppBarButtonText", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Menu Item.
-        /// </summary>
-        public static string AppBarMenuItemText {
-            get {
-                return ResourceManager.GetString("AppBarMenuItemText", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to MY APPLICATION.
-        /// </summary>
-        public static string ApplicationTitle {
-            get {
-                return ResourceManager.GetString("ApplicationTitle", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to type a message.
         /// </summary>
         public static string Comments_Hint {
