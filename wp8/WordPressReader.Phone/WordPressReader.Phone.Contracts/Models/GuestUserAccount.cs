bec58bd9b9298be52894df03535c2c859acea506
zmajcek <srdjan.bozovic@live.com>
2014-06-17 01:03:01 +0200

    fix: email validation

--- wp8/WordPressReader.Phone/WordPressReader.Phone.Contracts/Models/GuestUserAccount.cs
+++ wp8/WordPressReader.Phone/WordPressReader.Phone.Contracts/Models/GuestUserAccount.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace WordPressReader.Phone.Contracts.Models
 {
     public class GuestUserAccount
     {
+        public const string EMAIL_REGEX_PATTERN = @"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*@((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$";
+
         public string UserName { get; set; }
         public string Email { get; set; }
         
@@ -25,7 +28,8 @@ namespace WordPressReader.Phone.Contracts.Models
             get
             {
                 return !string.IsNullOrEmpty(UserName) &&
-                    !string.IsNullOrEmpty(Email);
+                    !string.IsNullOrEmpty(Email) &&
+                    Regex.IsMatch(Email, EMAIL_REGEX_PATTERN);
             }
         }
     }
